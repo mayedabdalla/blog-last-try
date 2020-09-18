9054aceb65f555835725c5a8bf3854fc7d1313be
mayedabdalla <mayed67@icloud.com>
2020-09-18 18:41:14 +0400

    Create layout

---
+++ components/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout({children}) {
+    return (
+        <>
+            <header>
+                <h1>Mayed</h1>
+            </header>
+            {children}
+        </>
+    )
+}
